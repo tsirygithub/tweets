2f84c936e0ba3f093cf7a058afabe8e4f41f1e42
Tsiry Andrian <tsiryandrian@gmail.com>
2022-01-19 15:08:34 +0300

    Nouveau projet Flask

---
+++ app/apis/tweets.py
@@ -0,0 +1,12 @@
+# app/apis/tweets.py
+# pylint: disable=missing-docstring
+
+from flask_restx import Namespace, Resource
+
+api = Namespace("tweets")
+
+
+@api.route("/hello")
+class TweetResource(Resource):
+    def get(self):
+        return "Hello from the 'tweets' namespace!"
